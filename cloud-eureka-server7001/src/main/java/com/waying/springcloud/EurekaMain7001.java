7bcf54c0a080c0c37287364845c9875c8308d7b8
Shmilysss <18395585776@163.com>
2020-05-31 20:08:19 +0800

    add Eureka

--- cloud-eureka-server7001/src/main/java/com/waying/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/waying/springcloud/EurekaMain7001.java
@@ -1,18 +1,18 @@
-package com.wqying.springcloud;
+package com.waying.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Author: wanqiangying
- * Date: 2020/5/30 23:36
+ * Date: 2020/5/31 18:36
  * Content:
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80 {
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class,args);
+        SpringApplication.run(EurekaMain7001.class,args);
     }
 }
